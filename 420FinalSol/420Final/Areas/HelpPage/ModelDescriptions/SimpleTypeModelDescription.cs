7ed6847e1aa29adf854ea6c69d9169dd1ee5772a
Tiffany Cole <Drakenshiinx@hotmail.com>
2021-05-30 14:23:10 -0700

    testing

---
+++ 420FinalSol/420Final/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _420Final.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ 420FinalSol/420Final/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_420Final.WebApiApplication" Language="C#" %>
