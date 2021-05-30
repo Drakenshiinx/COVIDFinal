7de8899a6e0befc6c1ab0581a668b98a0046faa3
Tiffany Cole <Drakenshiinx@hotmail.com>
2021-05-30 14:32:37 -0700

    added tiffbranch code

---
+++ 420FinalSol/420Final/Controllers/Query1Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _420Final.Controllers
+{
+    public class Query1Controller : ApiController
+    {
+        CovidDeathsDBEntities myDB = new CovidDeathsDBEntities();
+
+        [Route("api/covid/query1")]
+        [HttpGet]
+        public IHttpActionResult GetPositiveTests()
+        {
+            DateTime dateSpecific = new DateTime(2020, 06, 19);
+
+            var CovidTestPos = (from NursingHome in myDB.NursingHomeDeathsTables
+                                join Towns in myDB.TownCTDeathsTables
+                                on NursingHome.TownName equals Towns.TownName
+                                where Towns.Date == dateSpecific && NursingHome.Date == dateSpecific
+                                select new
+                                {
+                                    Towns.Date,
+                                    NursingHome.NursingHome,
+                                    NursingHome.TownName,
+                                    NursingHome.COVIDDeath,
+                                    Towns.ConfirmedCOVIDDeaths,
+                                    NursingHome.COVIDPositive,
+                                    Towns.TestPositive
+                                }).Distinct();
+
+            return Json(CovidTestPos);
+        }
+    }
+}
